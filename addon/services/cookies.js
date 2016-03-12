4bff0cf8c2dc56381ca579052386ce61a6933f6a
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-03-12 09:14:33 +0100

    read cookies from fastboot or document

--- addon/services/cookies.js
+++ addon/services/cookies.js
@@ -1,17 +1,47 @@
 import Ember from 'ember';
 
-const { inject: { service }, computed } = Ember;
+const { inject: { service }, computed, A } = Ember;
 
 export default Ember.Service.extend({
   fastboot: service(),
 
-  all: computed(function() {
-    const fastbootCookies = this.get('fastboot.cookies');
+  _isFastboot: computed.notEmpty('fastboot._fastbootInfo'),
+
+  _documentCookies: computed(function() {
+    const all = document.cookie.split(';');
+
+    return A(all).reduce((acc, cookie) => {
+      const [key, value] = cookie.split('=');
+      acc[key] = value;
+      return acc;
+    }, {});
+  }).volatile(),
+
+  _all: computed(function() {
+    if (this.get('_isFastboot')) {
+      return this.get('fastboot.cookies');
+    } else {
+      return this.get('_documentCookies');
+    }
+  }).volatile(),
+
+  read(name) {
+    const all = this.get('_all');
+
+    if (name) {
+      return all[name];
+    } else {
+      return all;
+    }
+  }/*,
+
+  write(name, value) {
+    const fastbootCookies = this.get('_fastbootCookies');
 
     if (fastbootCookies) {
-      return fastbootCookies;
+      
     } else {
-      return document.cookie;
+      
     }
-  }).volatile()
+  }*/
 });
--- tests/dummy/app/controllers/application.js
+++ tests/dummy/app/controllers/application.js
@@ -1,7 +1,17 @@
 import Ember from 'ember';
 
-const { inject: { service } } = Ember;
+const { inject: { service }, computed, keys } = Ember;
 
 export default Ember.Controller.extend({
-  cookies: service()
+  cookies: service(),
+
+  allCookies: computed(function() {
+    const cookies = this.get('cookies').read();
+    return keys(cookies).reduce((acc, key) => {
+      const value = cookies[key];
+      acc.push({ name: key, value });
+
+      return acc;
+    }, []);
+  }).volatile()
 });
