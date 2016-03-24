ccfa72eebec3e0a399e5287de2b928dcb1675cbd
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2016-03-24 18:45:10 +0100

    allow setting cookie options

--- addon/services/cookies.js
+++ addon/services/cookies.js
@@ -1,6 +1,6 @@
 import Ember from 'ember';
 
-const { inject: { service }, computed, A } = Ember;
+const { inject: { service }, computed, A, isEmpty, typeOf } = Ember;
 
 export default Ember.Service.extend({
   fastboot: service(),
@@ -11,8 +11,10 @@ export default Ember.Service.extend({
     const all = document.cookie.split(';');
 
     return A(all).reduce((acc, cookie) => {
-      const [key, value] = cookie.split('=');
-      acc[key] = value;
+      if (!isEmpty(cookie)) {
+        const [key, value] = cookie.split('=');
+        acc[key] = value.trim();
+      }
       return acc;
     }, {});
   }).volatile(),
@@ -36,12 +38,35 @@ export default Ember.Service.extend({
   },
 
   write(name, value, options = {}) {
+    Ember.assert('Cookies cannot be set to be HTTP-only as those would not be accessible by the Ember.js application itself when running in the browser!', !options.httpOnly);
+    Ember.assert("Cookies cannot be set as signed as signed cookies would not be modifyable in the browser as it has no knowledge of the express server's signing key!", !options.signed);
+    Ember.assert("Cookies cannot be set with both maxAge and an explicit expiration time!", isEmpty(options.expires) || isEmpty(options.maxAge));
+
     if (this.get('_isFastboot')) {
+      if (!isEmpty(options.maxAge)) {
+        options.maxAge = options.maxAge * 1000;
+      }
       const response = this.get('fastboot._fastbootInfo.response');
-      response.cookie(name, value);
+      response.cookie(name, value, options);
     } else {
       const encodedValue = encodeURIComponent(value);
-      const cookie = `${name}=${encodedValue}`;
+      let cookie = `${name}=${encodedValue}`;
+
+      if (!isEmpty(options.domain)) {
+        cookie = `${cookie}; domain=${options.domain}`;
+      }
+      if (typeOf(options.expires) === 'date') {
+        cookie = `${cookie}; expires=${expires.toUTCString()}`;
+      }
+      if (!isEmpty(options.maxAge)) {
+        cookie = `${cookie}; max=${options.maxAge}`;
+      }
+      if (!isEmpty(options.secure)) {
+        cookie = `${cookie}; secure`;
+      }
+      if (!isEmpty(options.path)) {
+        cookie = `${cookie}; path=${options.path}`;
+      }
       document.cookie = cookie;
     }
   }
