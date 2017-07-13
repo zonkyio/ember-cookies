34b170b0da827246e1ea1b672261ed3f867ae563
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2017-07-13 21:13:57 +0200

    update ember-cli et al

--- testem.js
+++ testem.js
@@ -1,13 +1,13 @@
 /* eslint-env node */
 
 module.exports = {
-  'framework': 'mocha',
-  'test_page': 'tests/index.html?hidepassed',
-  'disable_watching': true,
-  'launch_in_ci': [
+  framework: 'mocha',
+  test_page: 'tests/index.html?hidepassed',
+  disable_watching: true,
+  launch_in_ci: [
     'Chrome'
   ],
-  'launch_in_dev': [
+  launch_in_dev: [
     'Chrome'
   ]
 };
