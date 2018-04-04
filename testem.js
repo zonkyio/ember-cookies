c5cc471346ba5f99ceda36112e18ba4a430d9e01
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2018-04-04 18:41:32 +0200

    test in Firefox on CI

--- testem.js
+++ testem.js
@@ -5,12 +5,19 @@ module.exports = {
   test_page: 'tests/index.html?hidepassed',
   disable_watching: true,
   launch_in_ci: [
-    'Chrome'
+    'Chrome',
+    'Firefox'
   ],
   launch_in_dev: [
     'Chrome'
   ],
   browser_args: {
+    Firefox: {
+      mode: 'ci',
+      args: [
+        '-headless'
+      ]
+    },
     Chrome: {
       mode: 'ci',
       args: [
